3329d6257cb9cc842eb93b44ff5113df7c4b4850
Yakobus Damar <yakobus.damar@gmail.com>
2021-12-08 14:06:18 +0700

    First Commit

---
+++ mainApp/views.py
@@ -0,0 +1,21 @@
+from mainApp.forms import *
+from mainApp.models import *
+from django.shortcuts import get_list_or_404, render, redirect
+from .forms import *
+from .models import *
+from django.http import HttpResponse, JsonResponse
+from django.db.models import RestrictedError, Sum, Q, F
+from django.db.models.functions import Coalesce
+from django.contrib.auth.decorators import login_required
+from django import template
+register = template.Library()
+
+@register.filter
+def running_total(sales_list):
+    return sum(d.get('jumlah') for d in sales_list)
+
+loginpage = 'login'
+
+@login_required(login_url=loginpage)
+def index(request):
+    return render(request,'home.html')
